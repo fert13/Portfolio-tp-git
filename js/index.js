be686f8bd1810030ed79ee0827500aa668440d47
fert13 <nefertiti-ghislaine.dadjo@etudiant.univ-reims.fr>
2025-03-07 18:43:03 +0100

    Add

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ categories.forEach(category => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
+    navigator.serviceWorker.register('/js/sw.js').then((registration) => {
       console.log('Service Worker enregistré avec succès:', registration);
     }).catch((error) => {
       console.log('Échec de l\'enregistrement du Service Worker:', error);
